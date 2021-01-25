6e1161c88a5047d0114706d86a7a738161be78c0
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-25 15:50:56 -0300

    aula 04, primeiros passos, e primeiros 3 desafios

---
+++ desafios/desafio02.py
@@ -0,0 +1,5 @@
+dia = input('Dia: ')
+mes = input('Mês: ')
+ano = input('Ano: ')
+
+print('Você nasceu em',dia,'de',mes,'de',ano,'. Correto?')
---
+++ desafios/desafio03.py
@@ -0,0 +1,5 @@
+n1 = input('1st Número: ')
+n2 = input('2nd Número: ')
+res = n1+n2
+
+print('A soma de',n1,'e',n2,'é',res)
