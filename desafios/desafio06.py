b3fd15965774b2b2e7a93b5e402fccd8f50106ef
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-26 13:07:55 -0300

    resolvendo o exercicio 06

---
+++ desafios/desafio06.py
@@ -0,0 +1,9 @@
+print('Vamos te mostrar algumas propriedades de um número!')
+
+n = int(input('Digite um número: '))
+
+d = n*2
+t = n*3
+r = n**(1/2)
+
+print('O dobro é {} \nO triplo é {} \nA raiz quadrada é {}'.format(d,t,r))
