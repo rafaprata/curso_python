9b3d2625704845adc3b783af058680e797e57851
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-01-26 12:56:41 -0300

    resolvendo o exercicio 5

---
+++ desafios/desafio05.py
@@ -0,0 +1,7 @@
+print('Descubra o antecessor e o sucessor de um número!')
+n1 = int(input('Digite um valor: '))
+
+a = n1-1
+s = n1+1
+
+print('Antecessor {} \nNúmero {} \nSucessor {}'.format(a,n1,s))
