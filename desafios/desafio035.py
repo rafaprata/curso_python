6905be713588e905dc253c9c76013e57fd73c242
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-03-01 14:07:26 -0300

    melhorando a condicao com base na aula de resolução

--- desafios/desafio035.py
+++ desafios/desafio035.py
@@ -2,9 +2,7 @@ n1 = float(input('Digite uma medida: '))
 n2 = float(input('Digite outra medida: '))
 n3 = float(input('Digite a última medida: '))
 
-comp = [n1, n2, n3]
-comp.sort()
-if comp[2] >= comp[1]+comp[0]:
-    print('Não é possível desenhar um triângulo com as medidas\n {}\n {}\n {}'.format(n1, n2, n3))
+if n1<n2+n3 and n2<n1+n3 and n3<n1+n2:
+    print('É possível formar um TRIÂNGULO')
 else:
-    print('As medidas formam um TRIÂNGULO!')
+    print('As medidas NÃO formam um TRIÂNGULO!')
